bbc67ab8a165c2935465357b4aa41eb3471d9d77
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-08-03 11:31:34 +0700

    Initialize

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); // ExpressJS as a underlying platform API.
+  await app.listen(3000);
+}
+bootstrap();
