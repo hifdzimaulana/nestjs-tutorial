b18ede44630abeb11eb38bfbfd62ef6325a15d43
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-08-03 15:58:54 +0700

    set up swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule); // ExpressJS as a underlying platform API.
+
+  // Swagger API
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/docs', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
